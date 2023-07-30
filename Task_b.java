d6643617dac32f3b8882d3c91d92238da83a3834
Kavindu95 <kavindueng95@gmail.com>
2023-07-30 12:46:07 +0530

    border update

--- Task_b.java
+++ Task_b.java
@@ -5,11 +5,15 @@ public class Task_b {
         final String COLOR2 = "\033[33;1m";
         final String RESET2 = "\033[30;0m";
 
-        System.out.printf("+%15s+%5s+ \n","---------------","------");
-        System.out.printf("|%s%-15s%s| %s%-5s%s| \n",COLOR3,"NAME",RESET3,COLOR3,"AGE",RESET3);
-        System.out.printf("+%15s+%5s+ \n","---------------","------");
-        System.out.printf("|%-15s| %s%-5s%s| \n","Alice",COLOR2,"24",RESET2);
-        System.out.printf("|%-15s| %s%-5s%s| \n","Bob",COLOR2,"30",RESET2);
-        System.out.printf("+%15s+%5s+ \n","---------------","------");
+        final String ORANGE ="\u001B[38;5;214m";
+        final String ORANGER ="\u001B[0m";
+
+
+        System.out.printf("%s+%15s+%5s+%s \n",ORANGE,"---------------","------",ORANGER);
+        System.out.printf("%s|%s%s%-15s%s%s|%s %s%-5s%s%s|%s \n",ORANGE,ORANGER,COLOR3,"NAME",RESET3,ORANGE,ORANGER,COLOR3,"AGE",RESET3,ORANGE,ORANGER);
+        System.out.printf("%s+%15s+%5s+%s \n",ORANGE,"---------------","------",ORANGER);
+        System.out.printf("%s|%s%-15s%s|%s %s%-5s%s%s|%s \n",ORANGE,ORANGER,"Alice",ORANGE,ORANGER,COLOR2,"24",RESET2,ORANGE,ORANGER);
+        System.out.printf("%s|%s%-15s%s|%s %s%-5s%s%s|%s \n",ORANGE,ORANGER,"Bob",ORANGE,ORANGER,COLOR2,"30",RESET2,ORANGE);
+        System.out.printf("%s+%15s+%5s+%s \n",ORANGE,"---------------","------",ORANGER);
     }
 }
--- TestDemo.java
+++ TestDemo.java
@@ -3,21 +3,7 @@ public class TestDemo {
   
     public static void main(String[] args) {
         
-        final int id = 1;
-        String name = "Kasun Sampath";
-        double avgMarks = 65.67999;
-
-        System.out.printf("id=S%04d, name=%s, avgMarks=%.2f \n", id, name, avgMarks);
-        System.out.println();
-
-        final String LINE = "+%6s+%1$15s+%1$6s+ \n";
-        final String RECORD = "|SK-%03d|%-15s|%6.2f| \n";
-
-        System.out.printf(LINE, "");
-        System.out.printf(RECORD, id, name, avgMarks);
-        System.out.printf(LINE, "");
-        System.out.printf(RECORD, 2, "Nuwan Ramindu", 62.2225);
-        System.out.printf(LINE, "");
+        System.out.println("\1u00B[38;5;214mThis is orange text!\u001B[0m");
         
     }
 }
