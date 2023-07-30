7c8542cf5155de38dc8833bd544d9bb2f2898f46
Kavindu95 <kavindueng95@gmail.com>
2023-07-30 14:17:01 +0530

    newyork second update

--- Task_c.java
+++ Task_c.java
@@ -6,12 +6,14 @@ public class Task_c {
         final String RESET3 = "\033[30;0m";
         final String COLOR4 = "\033[37;1m";
         final String RESET4 = "\033[30;0m";
+        final String COLOR5 = "\033[32;1m";
+        final String RESET5 = "\033[30;0m";
 
         System.out.printf("%s+%15s+%10s+%12s+%s \n",ORANGE,"---------------","-----------","-------------",ORANGER);
         System.out.printf("%s|%s%s%-15s%s%s|%s %s%-10s%s%s|%s %s%-12s%s%s| \n",ORANGE,ORANGER,COLOR3,"CITY",RESET3,ORANGE,ORANGER,COLOR3,"STATE",RESET3,ORANGE,ORANGER,COLOR3,"POPULATION",RESET3,ORANGE,ORANGER);
         System.out.printf("%s+%15s+%10s+%12s+%s \n",ORANGE,"---------------","-----------","-------------",ORANGER);
         System.out.printf("%s|%s%s%-15s%s%s|%s%s%-10s%s%s |%s%s%-12s%s%s | \n",ORANGE,ORANGER,COLOR4,"Los Angeles",RESET4,ORANGE,ORANGER,COLOR4,"California",RESET4,ORANGE,ORANGER,COLOR4,"3,966,936",RESET4,ORANGE,ORANGER);
-        System.out.printf("%s|%s%s %6s%s%-15s%s%s|%s%s%-10s%s%s |%s%s%-12s%s%s | \n",ORANGE,ORANGER,COLOR4,"New",RESET4,COLOR3,"York",RESET3,ORANGE,ORANGER,COLOR4,"New York",RESET4,ORANGE,ORANGER,COLOR4,"8,336,817",RESET4,ORANGE,ORANGER);
+        System.out.printf("%s|%s%s%-5s%s%s%-10s%s%s|%s%s%-5s%s%s%-5s%s%s |%s%s%-12s%s%s | \n",ORANGE,ORANGER,COLOR5,"New",RESET5,COLOR4,"York",RESET4,ORANGE,ORANGER,COLOR5,"New",RESET5,COLOR4,"York",RESET4,ORANGE,ORANGER,COLOR4,"8,336,817",RESET4,ORANGE,ORANGER);
         System.out.printf("%s+%15s+%10s+%12s+%s \n",ORANGE,"---------------","-----------","-------------",ORANGER);
 
     }
