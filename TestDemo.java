7879d4b82bebb03b58fd3df196e3b4faff4bc8da
Kavindu95 <kavindueng95@gmail.com>
2023-07-30 16:39:23 +0530

    Completed

--- TestDemo.java
+++ TestDemo.java
@@ -2,8 +2,6 @@ public class TestDemo {
 
   
     public static void main(String[] args) {
-        
-        System.out.print("\u001b[31mThis is red text\u001b[0m");
-        
+        System.out.println("Welcome to DEP");    
     }
 }
